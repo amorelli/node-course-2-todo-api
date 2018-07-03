035bf76b48f9a4ad0878775080b63dfaa01fed9f
Adam Morelli <amorelli@gmail.com>
2018-07-02 20:22:02 -0400

    Add update script.

---
+++ playground/mongodb-update.js
@@ -0,0 +1,23 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+	if (err) {
+		return console.log('Could not connect to MongoDB server.');
+	}
+	console.log('Connected to MongoDB server.');
+	const db = client.db('TodoApp');
+
+	db.collection('Users')
+	.findOneAndUpdate(
+	{ _id: new ObjectID('5b3a83ec6cc26f10cc1558b7') }, 
+	{ $set:{ name: 'Adam'}, $inc:{ 'age': 1 } }, 
+	{ returnOriginal: false })
+	.then((res) => {
+		console.log(res);
+	});
+
+
+
+	// client.close();
+});
