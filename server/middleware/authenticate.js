2d1336f41c05f8f4d5866989a9539b63bbbff139
Adam Morelli <amorelli@gmail.com>
2018-08-15 14:15:11 -0400

    Updated route testing with async/await

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -1,11 +1,11 @@
 var {User} = require('./../models/user');
-/* It makes it private because it searches for a user in our database that matches the token sent in the request header. 
+/* Searches for a user in our database that matches the token sent in the request header. 
 A user only receives a token once they've logged in, so if we find a token in our database that matches, 
 we know the user is logged in and we can then proceed with sending them to the route handler. */
-var authenticate = (req, res, next) => {
+var authenticate = async (req, res, next) => {
 	var token = req.header('x-auth');
-
-	User.findByToken(token).then((user) => {
+	try {
+		const user = await User.findByToken(token);
 		if (!user) {
 			return Promise.reject();
 		}
@@ -13,9 +13,9 @@ var authenticate = (req, res, next) => {
 		req.user = user;
 		req.token = token;
 		next();
-	}).catch((e) => {
+	} catch(e) {
 		res.status(401).send();
-	});
+	};
 };
 
 module.exports = {authenticate};
